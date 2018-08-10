828c2cf56dff3415d55d4ac2eeb1a2f488c83307
Tojikan <andrew.eqian@gmail.com>
2018-08-09 20:44:53 -0400

    finished commiting everything

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+//gulp.js configuration 
+
+//modules
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+//sass processing
+//when sass is run, compoiles the sass code into the css folder. 
+gulp.task('sass', function(){
+    return gulp.src('scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
+});
+
+
+//browser syncing
+//creates a task called serve that creates a server at root ./ then uses gulp.watch to watch the sass file to call sass if any changes
+//All html files. reloads browsers if html is updated
+gulp.task('serve', ['sass'], function(){
+
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./scss/styles.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+//when gulp is run at command line within the project, call the serve task by default so you don't have to call serve explicitly
+gulp.task('default', ['serve']);
